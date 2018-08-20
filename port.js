5cc5bc5580eb03e9cedb5913f46244aad8823ed7
Jo60 <Jo60@users.noreply.github.com>
2018-08-20 03:04:54 -0400

    ok

---
+++ port.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+/*alert('loaded');*/
+    
+var preview = function(){
+
+$('#firstSite').hover(function(){
+	
+ $("#preview").attr("src","firstSite.jpg");
+    });
+    
+$('#imran').hover(function(){
+	
+ $("#preview").attr("src","imran.jpg");
+    });
+    
+$('#vicrit').hover(function(){
+	
+ $("#preview").attr("src","victoria.jpg");
+    });
+    
+$('#ray').hover(function(){
+	
+ $("#preview").attr("src","wip.png");
+ 
+});
+   
+    
+};
+
+
+
+
+preview();    
+    
+});
